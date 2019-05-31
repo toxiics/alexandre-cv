a9a7878ed0c4c4fceea1de5ebfa8776635abd443
acaillard <alexandre.caillard@gfi.fr>
2019-05-31 11:47:25 +0200

    init navbar + style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar'
+import TypoGraphy from '@material-ui/core/Typography'
+import NavBar from './NavBar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AppBar color="primary" position="static">
+          <Toolbar id="toolbar">
+            <TypoGraphy variant="title"
+              color="inherit"
+            >
+              <h1 class="nameHeader">Caillard Alexandre</h1>
+              <h3 class="jobHeader">Développeur Web</h3>
+            </TypoGraphy>
+            <NavBar />
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 }
 
 export default App;
